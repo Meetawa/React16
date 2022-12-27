870d7b0dba4195a3d5868c1e0150fced5a45ce3a
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2022-12-27 15:38:00 +0530

    render issues fixed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
