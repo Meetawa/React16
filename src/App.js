535cb50c9b285e2fe2dbee9cbd702b54336f8dd8
RAHUL <rahulmeetawa@gmail.com>
2023-02-26 13:23:36 +0530

    React 16.8.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import DropdownMenuDefaultExample from './DropdownMenuDefaultExample';
 
 function App() {
   return (
     <div>
       <h1>RAHUL SAINI</h1>
-     <DropdownMenuDefaultExample></DropdownMenuDefaultExample>   
     </div>
   );
 }
